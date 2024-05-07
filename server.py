6fb3c0eb42ee43e6742134b87e52affc34b505fa
Adeola Adekoyejo <adekoyejoadeola@gmail.com>
2024-05-07 16:05:36 +0100

    feat: complete deployment

--- server.py
+++ server.py
@@ -3,27 +3,27 @@
     localhost:5000.
 '''
 from flask import Flask, render_template, request 
-# from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
-
-#Initiate the flask app : TODO
-app = Flask(__name__)
+app = Flask('Sentiment Analyzer')
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
-    ''' This code receives the text from the HTML interface and 
-        runs sentiment analysis over it using sentiment_analysis()
-        function. The output returned shows the label and its confidence 
-        score for the provided text.
-    '''
-    # TODO
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = sentiment_analyzer(text_to_analyze)
+    label = response['label']
+    score = response['score']
+    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+
+
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
+      ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
+      return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=5050)
